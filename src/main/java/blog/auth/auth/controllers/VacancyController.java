a1f12fce09bb01a9dd9c83d0a651da1f2941f2ea
Mir1yasu <miriyasu31@gmail.com>
2025-05-29 15:46:43 +0500

    Fixed endpoint

--- src/main/java/blog/auth/auth/controllers/VacancyController.java
+++ src/main/java/blog/auth/auth/controllers/VacancyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/vacancies")
+@RequestMapping("/getJobStats")
 @RequiredArgsConstructor
 public class VacancyController {
 
